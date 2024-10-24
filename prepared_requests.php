dbb5cf77352e47b40ae471a1414ed07a5c995380
kimiis <kimi.0208@outlook.fr>
2024-10-24 13:14:50 +0200

    prepared_queries file

---
+++ prepared_requests.php
@@ -0,0 +1,65 @@
+<?php
+
+function getDbConnection() {
+    $host = 'localhost';
+    $dbname = 'hackaton';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Erreur de connexion : " . $e->getMessage();
+        exit();
+    }
+}
+
+function createUser($last_name, $first_name, $date_of_birth, $email, $hashedPassword) {
+    $pdo = getDbConnection();
+
+    $sql = "INSERT INTO users (first_name, last_name, date_of_birth, email, password) VALUES (:first_name, :last_name, :date_of_birth, :email, :password)";
+    $stmt = $pdo->prepare($sql);
+
+    if ($stmt->execute([
+        ':last_name' => $last_name,
+        ':first_name' => $first_name,
+        ':date_of_birth' => $date_of_birth,
+        ':email' => $email,
+        ':password' => $hashedPassword
+    ])) {
+        return $pdo->lastInsertId();
+    } else {
+        return false;
+    }
+}
+
+function createRole($roleName) {
+    $pdo = getDbConnection();
+
+    $sql = "INSERT INTO roles (role_name) VALUES (:role_name)";
+    $stmt = $pdo->prepare($sql);
+
+    if ($stmt->execute([
+        ':role_name' => $roleName
+    ])) {
+        return $pdo->lastInsertId();
+    } else {
+        return false;
+    }
+}
+
+function assignRoleToUser($userId, $roleId) {
+    $pdo = getDbConnection();
+
+    $sql = "INSERT INTO user_roles (id_user, id_role) VALUES (:id_user, :id_role)";
+    $stmt = $pdo->prepare($sql);
+
+    return $stmt->execute([
+        ':id_user' => $userId,
+        ':id_role' => $roleId
+    ]);
+}
+
+
