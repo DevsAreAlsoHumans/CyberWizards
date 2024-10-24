fd2f8ec4976b97e42d0f168db27cf04ed8c69a10
irma <irma79200@gmail.com>
2024-10-24 14:52:17 +0200

    function disconnect done

--- disconnection.php
+++ disconnection.php
@@ -1,11 +1,19 @@
 <?php
 
 if(array_key_exists('ButtonLogout', $_POST)) {
-        buttonlogout();
-    }
-    function buttonlogout() {
-
-        echo "Vous êtes déconnecté !";
-    }
-
+    //delete all variables session. 
+    $_SESSION = array(); 
+ 
+    // If you want delete the cookie session, destroy the cookie.
+    if (ini_get("session.use_cookies")) { 
+        $params = session_get_cookie_params(); 
+        setcookie(session_name(), '', time() - 42000, 
+        $params["path"], $params["domain"],  
+        $params["secure"], $params["httponly"] 
+    ); 
+} 
+    session_destroy(); 
+    header("Location: home.php"); 
+    exit(); 
+}  
 ?>
